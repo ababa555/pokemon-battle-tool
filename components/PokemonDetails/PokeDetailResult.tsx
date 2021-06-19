543d8b89cf1dfdc05838b904b2e582812fdeb426
ababa555 <japan14144@yahoo.co.jp>
2021-06-19 22:26:54 +0900

    不要な記述を削除

--- components/PokemonDetails/PokeDetailResult.tsx
+++ components/PokemonDetails/PokeDetailResult.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import dynamic from 'next/dynamic'
 
-import { makeStyles, Theme } from '@material-ui/core/styles';
-import { createStyles } from '@material-ui/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
